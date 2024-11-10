2a21f3cd58039945b43e7283431076588502efb8
Speedboat225 <neilgrover8550@gmail.com>
2024-11-10 20:25:54 +0530

    Add files via upload
    felt unwell while doing this one

---
+++ kfjfjjde.py
@@ -0,0 +1,72 @@
+# maths thingy
+
+first = 10
+seocnd = 200
+third = 333
+fourth = 44
+fifth = 5
+
+z= ((first+seocnd)*(third/fourth))**fifth
+
+print(z)
+
+finput = int(input("enter a number\n"))
+sinput = int(input("enter a number 2\n"))
+tinput = int(input("enter a number 3\n"))
+foinput = int(input("enter a number 4\n"))
+fiinput = int(input("enter a number 5\n"))
+
+
+resultofalltheseinputsare = ((finput/foinput)-(sinput**tinput)//fiinput)/2
+
+print(resultofalltheseinputsare)
+
+# divise thimng
+
+numbaone = int(input("Enter a number to be divided.\n"))
+
+numbatoo = int(input("Enter the number to be divided by.\n"))
+
+ressuaalalltkakktkt = numbaone%numbatoo
+
+print(ressuaalalltkakktkt)
+
+if ressuaalalltkakktkt == 0:
+    print("Divisible!")
+
+else:
+    print("Indivisible.")
+
+numbers=40
+meanp=38
+wrong=36
+correctnum=56
+#sum of 40 numbers
+sum=meanp*numbers
+print(sum)
+
+num2=sum-((wrong)+(correctnum))
+print(num2)
+
+actualm=num2/numbers
+print(actualm)
+
+# cycle
+
+cyclist1 = 10
+cyclist2 = 20
+cyclist3 = 30
+
+print (cyclist1, cyclist2, cyclist3)
+
+avgspeed = (cyclist1 + cyclist2 + cyclist3)/3
+print("the average esppeed of the cycxlists include:", avgspeed)
+
+if cyclist1 < avgspeed:
+    print("The first cyclist is below average speed.")
+
+elif cyclist1 < avgspeed:
+    print("The first cyclist is below average speed.")
+
+elif cyclist1 < avgspeed:
+    print("The first cyclist is below average speed.")
