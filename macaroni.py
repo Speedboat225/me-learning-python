5fae6da49f76e20adbe9d421ae0780f262fc4b0d
Speedboat225 <neilgrover8550@gmail.com>
2024-11-23 20:20:15 +0530

    Add files via upload

---
+++ macaroni.py
@@ -0,0 +1,43 @@
+y="hahaha"
+
+for a in y:
+    print(a)
+
+
+for e in range(11):
+    print(e)
+
+
+for q in range(1,8):
+    print(q)
+
+for f in range(5,25,2):
+    print(f)
+
+# Sum of whole numbers
+
+tootsieroll = int(input("Enter a whole number."))
+
+threemusketeers =0
+
+for u in range(1,tootsieroll):
+    threemusketeers=threemusketeers+u
+    print("\n Sum: ",threemusketeers)
+
+# reverse string
+
+toblerone = input("enter text, NOW.\n")
+
+empty = ""
+
+for xij3ijij in toblerone:
+    empty = xij3ijij + empty
+
+print(empty)
+
+poland = int(input("enter numeros, NOW.\n"))
+
+
+
+for aajejajea in range(poland,0,-1):
+    print(aajejajea)
