92740996d8d9aaa496f6765b7ec34e81540d0dee
Speedboat225 <neilgrover8550@gmail.com>
2024-12-20 20:16:57 +0530

    Add files via upload

---
+++ fdgtdtgf.py
@@ -0,0 +1,8 @@
+print("Right Angle Triangle with Delta")
+judgeman = int(input("row row the boat (how many rows do u want)\n"))
+
+for k in range(judgeman):
+    for o in range(k+1):
+        print("∆",end=" ")
+
+    print()
