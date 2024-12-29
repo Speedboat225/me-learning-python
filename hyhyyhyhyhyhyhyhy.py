27c7983606e0d33a1cef1df477876e4ebc09608c
Speedboat225 <neilgrover8550@gmail.com>
2024-12-29 20:14:27 +0530

    Add files via upload

---
+++ hyhyyhyhyhyhyhyhy.py
@@ -0,0 +1,37 @@
+anjjfhf = input("entrerr te xt\n").strip()
+for e in anjjfhf:
+    if e=="A"or e== "a":
+       print ("your text has the letter A in it!1!!!!11!!")
+       break
+
+    else:
+        while True:
+            print("William where are the A's.")
+
+jfjjfjfj = int(input("entrerr numb ereerrqrerqr\n"))
+for a in range(jfjjfjfj):
+    if a%15 == 0:
+        pass
+    elif a%20 == 0:
+        print("twist")
+    elif a%5 == 0:
+        print("fizz")
+    elif a%3 == 0:
+        print("buzz")
+
+    else:
+        print(a)
+
+fwfwwef = int(input("write nuuuffmbreer agaeijnv (1-10 this time!!!!!!!!!!!!!!)"))
+
+while fwfwwef>0:
+
+    if fwfwwef > 10:
+        print("bro.")
+        break
+        
+    else:
+        fwfwwef=fwfwwef-1
+        if fwfwwef==5:
+            continue
+        print(fwfwwef)
