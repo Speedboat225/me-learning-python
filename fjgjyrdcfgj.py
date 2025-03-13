43da42edf4002819d9aacb70cc4f5cbc48ecd84c
Speedboat225 <neilgrover8550@gmail.com>
2025-03-13 20:47:59 +0530

    knjlkllk

---
+++ fjgjyrdcfgj.py
@@ -0,0 +1,28 @@
+set1={5, 7, 7, 9, 3}
+print("Set1 is ",set1)
+
+set2 = {3, "uyilyo", "uyilyo", True, 6.5454544545454545454545454}
+print("Set2 is ",set2)
+
+set233434 = {1, 2, 3, 4, 3, 2, 4}
+print("Set3 is ",set233434)
+
+lsiitisitisiitsitisittististit = set([1, 2, 3, 2])
+print("fke2dfrujn2", lsiitisitisiitsitisittististit)
+
+execution = set([0, 1, 3, 4, 5])
+kjhgfd = execution.pop()
+print("fijoijiojfijo", execution)
+
+set7890 = {"green", "blue"}
+set1337 = {"blue", "yellow", "leet"}
+
+set123456 = set7890.intersection(set1337)
+
+print("does anyone actually read these?", set123456)
+
+xdf = [1, 3, 5, 3, 7, 9, 3]
+sjsj = xdf.count(3)
+print(sjsj)
+xdf.reverse()
+print(xdf)
